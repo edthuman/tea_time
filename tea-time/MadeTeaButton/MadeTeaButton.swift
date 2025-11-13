68a697fec9de01a204847ee61fad414a1e07b527
Edward Irvine <edwardadirvine@aol.com>
2025-11-13 22:20:55 +0000

    Remove unnecessary comments

--- tea-time/MadeTeaButton/MadeTeaButton.swift
+++ tea-time/MadeTeaButton/MadeTeaButton.swift
@@ -5,11 +5,6 @@ struct MadeTeaButton: View {
         VStack {
             Button {
                 Task {
-                    /*
-                     10 second delay added
-                     - For simulator - only shows if app is in the background
-                     - On actual phone - only shows the notifcations when the app is rebuilt
-                     */
                     await beginTimer(length: 10)
                 }
             } label: {
--- tea-time/tea_timeApp.swift
+++ tea-time/tea_timeApp.swift
@@ -1,10 +1,3 @@
-//
-//  tea_timeApp.swift
-//  tea-time
-//
-//  Created by Edward Irvine on 31/08/2025.
-//
-
 import SwiftUI
 
 @main
