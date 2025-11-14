8f7bb764c0ec19a89de84b61baf633a4331315e6
Edward Irvine <edwardadirvine@aol.com>
2025-11-14 20:18:14 +0000

    Update beginTimer to create both start and finish notifications

--- tea-time/MadeTeaButton/MadeTeaButton-helpers.swift
+++ tea-time/MadeTeaButton/MadeTeaButton-helpers.swift
@@ -10,36 +10,54 @@ func printWithNewlineAbove(input: Any) {
 /// use of seconds means incomplete - e.g. half/quarter minutes are easier to generate
 func beginTimer(length: Double) async {
     printWithNewlineAbove(input: "Creating a timer for \(length) seconds...")
-
-    let request = createNotification(timeDelay: length)
-
-    // Schedule the request with the system.
-    let notificationCenter = UNUserNotificationCenter.current()
-    notificationCenter.removeAllPendingNotificationRequests()
     
+    // notification to show that timer has started
+    let startNotif = createNotification(
+        title: "Timer started!",
+        description: "Tea minus \(length) minutes...",
+        playSound: false,
+        timeDelay: nil
+    )
+    
+    // notification to show that timer has finished
+    let endNotif = createNotification(
+        title: "Tea time!",
+        description: "🙂‍↕️🙂‍↕️🙂‍↕️",
+        playSound: true,
+        timeDelay: length
+    )
+
+    // send the notifications requests
+    let notificationCenter = UNUserNotificationCenter.current()
     do {
-        try await notificationCenter.add(request)
-        printWithNewlineAbove(input: "Notification added!")
+        try await notificationCenter.add(startNotif)
+        try await notificationCenter.add(endNotif)
     } catch {
         printWithNewlineAbove(input: "Error adding notification: \(error)")
     }
-    
-    // print requests that are pending in the notification center
-    let requests = await notificationCenter.pendingNotificationRequests()
-    printWithNewlineAbove(input: requests)
 }
 
-func createNotification(timeDelay: Double) -> UNNotificationRequest {
+func createNotification(title: String, description: String, playSound: Bool, timeDelay: Double?) -> UNNotificationRequest {
     let uuid = UUID().uuidString
 
     let content = UNMutableNotificationContent()
-    content.title = "Tea Alert"
-    content.body = "15 minutes begins now..."
+    content.title = title
+    content.body = description
     
-    let soundName = UNNotificationSoundName(rawValue: "_mary.wav")
-    content.sound = UNNotificationSound(named: soundName)
+    if (playSound) {
+        // add default sound to notification config
+        let soundName = UNNotificationSoundName(rawValue: "_mary.wav")
+        content.sound = UNNotificationSound(named: soundName)
+    }
     
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeDelay, repeats: false)
-    let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+    if (timeDelay != nil) {
+        // send notification to be sent after timer length passes
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeDelay!, repeats: false)
+        let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+        return request
+    }
+    
+    // send notification immediately
+    let request = UNNotificationRequest(identifier: uuid, content: content, trigger: nil)
     return request
 }
