7d3f7739234086c86841482ef622ea235c5cd6d8
Edward Irvine <edwardadirvine@aol.com>
2026-02-28 20:23:41 +0000

    Revert "Add the ability to crop images chosen from the ImagePicker View"
    This reverts commit 7ea9fa76a63a69cbedb7645e57fe7b77532d1972.

--- tea-time/ImagePicker/ImagePicker.swift
+++ tea-time/ImagePicker/ImagePicker.swift
@@ -10,7 +10,6 @@ struct ImagePicker: UIViewControllerRepresentable {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         picker.sourceType = .photoLibrary
-        picker.allowsEditing = true
         return picker
     }
 
@@ -31,7 +30,7 @@ struct ImagePicker: UIViewControllerRepresentable {
 
         // Called when an image is selected
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-            if let uiImage = info[.editedImage] as? UIImage {
+            if let uiImage = info[.originalImage] as? UIImage {
                 // Save the selected image to the bound property
                 parent.selectedImage = uiImage
             }
