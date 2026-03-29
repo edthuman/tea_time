914c84152a2ed4e2fc6e7c6302d6dc7b0ad9b1d7
Edward Irvine <edwardadirvine@aol.com>
2026-03-30 00:02:56 +0100

    Add resetting of folderBeingEdited in state when add new folder button is clicked

--- tea-time/Folders/AddFolderButton/AddFolderButton.swift
+++ tea-time/Folders/AddFolderButton/AddFolderButton.swift
@@ -2,10 +2,12 @@ import SwiftUI
 import CoreData
 
 struct AddFolderButton: View {
+    @ObservedObject var state = appState
     @State private var isAdding: Bool = false
     
     var body: some View {
         Button {
+            state.setFolderBeingEdited(nil)
             isAdding.toggle()
         } label: {
             Image(systemName: "plus")
