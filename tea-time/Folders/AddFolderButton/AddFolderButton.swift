d4b987b1d5f97e603a4f631bf54b94211fb5b949
Edward Irvine <edwardadirvine@aol.com>
2026-04-12 22:57:50 +0100

    Update use of black to support users' dark/light mode general setting

--- tea-time/Folders/AddFolderButton/AddFolderButton.swift
+++ tea-time/Folders/AddFolderButton/AddFolderButton.swift
@@ -5,6 +5,8 @@ struct AddFolderButton: View {
     @ObservedObject var state = appState
     @State private var isAdding: Bool = false
     
+    let black: Color = Color("black_for_light")
+    
     var body: some View {
         Button {
             state.setFolderBeingEdited(nil)
@@ -12,11 +14,11 @@ struct AddFolderButton: View {
         } label: {
             Image(systemName: "plus")
                 .font(.system(size: 16, weight: .bold))
-                .foregroundStyle(.black)
+                .foregroundStyle(black)
                 .frame(width: 36, height: 36)
                 .overlay(
                     Circle()
-                        .stroke(Color.black, lineWidth: 2)
+                        .stroke(black, lineWidth: 2)
                 )
                 .contentShape(Circle())
         }
