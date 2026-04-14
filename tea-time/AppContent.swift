ab1a8fdaa70678296cb6297b4788a912c6a55255
Edward Irvine <edwardadirvine@aol.com>
2026-04-14 23:20:39 +0100

    Fix flipped logic for folder state check

--- tea-time/AppContent.swift
+++ tea-time/AppContent.swift
@@ -5,7 +5,7 @@ struct AppContent: View {
     @ObservedObject var state = appState
     
     var body: some View {
-        if state.folder != nil {
+        if state.folder == nil {
             Folders()
         } else {
             VStack {
