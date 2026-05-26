2aa4221bbacb92e8e73e3b0dbda0f1d132e7b28e
Edward Irvine <edwardadirvine@aol.com>
2026-05-27 00:30:08 +0100

    Update Button formatting in EditTimerForm to be consistent across Buttons in EditTimerForm

--- tea-time/Timers/EditTimerForm/EditTimerForm.swift
+++ tea-time/Timers/EditTimerForm/EditTimerForm.swift
@@ -374,14 +374,14 @@ struct EditTimerForm: View {
                 .padding(.bottom, 20)
                 
                 HStack {
-                    Button(action: resetState) {
-                        Text("Cancel")
-                            .foregroundStyle(.red)
+                    Button("Cancel") {
+                        resetState()
                     }
+                    .foregroundStyle(.red)
                     .padding(.trailing, 20)
                     
-                    Button(action: saveChanges) {
-                        Text("Confirm")
+                    Button("Confirm") {
+                        saveChanges()
                     }
                 }
                 .padding(.top, 5)
