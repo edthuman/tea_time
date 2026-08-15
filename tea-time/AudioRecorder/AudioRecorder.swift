b4712c8e77ab3b8160d92126b5324925e65cd89c
Edward Irvine <edwardadirvine@aol.com>
2026-08-15 20:56:38 +0100

    Update AudioRecorder to use AVAudioEngine instead of AVAudioRecorder to allow greater control over recording length

--- tea-time/AudioRecorder/AudioRecorder.swift
+++ tea-time/AudioRecorder/AudioRecorder.swift
@@ -1,120 +1,79 @@
 import AVFoundation
 import SwiftUI
 
-class RecorderDelegate: NSObject, AVAudioRecorderDelegate {
-    var onFinish: ((Bool) -> Void)?
+@Observable
+class AudioRecorder {
+    private var engine = AVAudioEngine()
+    private var audioFile: AVAudioFile?
     
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        DispatchQueue.main.async {
-            self.onFinish?(flag)
-        }
-    }
-}
-
-struct AudioRecorder: View {
-    @State var recorder: AVAudioRecorder?
-    @State var isRecording: Bool = false
-    @State var recordingStartTime: Date?
-    @State var recorderDelegate = RecorderDelegate()
+    var status: RecorderStatus = RecorderStatus.idle
+    var framesRecorded: AVAudioFrameCount = 0
+    var maximumFrames: AVAudioFrameCount = 0
     
-    let maxTimerLength = TimeInterval(5)
-    
-    func getSoundFileURL() throws -> URL {
-        let fileManager = FileManager.default
-        let documentDirectoryURLs = fileManager.urls(
-            for: .documentDirectory,
-            in: .userDomainMask
-        )
-
-        guard let documentDirectoryURL = documentDirectoryURLs.first else {
-            throw FileDirectoryErrors.documentDirectoryNotFound
-        }
-        let testFile: URL = documentDirectoryURL.appendingPathComponent("test-file.m4a")
-        
-        return testFile
-    }
-    
-    func getRecorder() {
-        let settings: [String: Any] = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 22050,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
+    func startRecording(outputURL: URL) {
+        status = RecorderStatus.transitioning
+        framesRecorded = 0
+        let session = AVAudioSession.sharedInstance()
         
         do {
-            recorder = try AVAudioRecorder(url: getSoundFileURL(), settings: settings)
-            recorder?.prepareToRecord()
-            recorder?.delegate = recorderDelegate
+        try session.setActive(true)
+
+        let format = engine.inputNode.outputFormat(forBus: 0)
+        maximumFrames = AVAudioFrameCount(30.0 * format.sampleRate)
+        
+            audioFile = try AVAudioFile(
+                forWriting: outputURL,
+                settings: format.settings
+            )
             
-        }
-        catch {
-            // Add better handling of error
-            printWithNewlineAbove(input: "Could not initialise AVAudioRecorder: \(error)")
+            engine.reset()
+            engine.inputNode.installTap(
+                onBus: 0,
+                bufferSize: 4096,
+                format: format,
+                block: { buffer, time in
+                    let bufferLength = buffer.frameLength
+                    
+                    let unusableBuffer = self.framesRecorded >= self.maximumFrames
+                    if (unusableBuffer) {
+                        return
+                    }
+                    
+                    let maxLengthExceeded = self.framesRecorded + bufferLength > self.maximumFrames
+                    if (maxLengthExceeded) {
+                        // Save partial buffer to audio file
+                        let requiredFrames = AVAudioFrameCount(self.maximumFrames - self.framesRecorded)
+                        buffer.frameLength = requiredFrames
+                    }
+                    
+                    do {
+                        try self.audioFile?.write(from: buffer)
+                        self.framesRecorded += buffer.frameLength
+                        
+                        if (self.framesRecorded >= self.maximumFrames) {
+                            DispatchQueue.main.async {
+                                self.stopRecording()
+                            }
+                        }
+                    } catch {
+                        printWithNewlineAbove(input: "Failed to write to file: \(error)")
+                    }
+                }
+            )
+            try engine.start()
+            status = RecorderStatus.recording
+        } catch {
+            status = RecorderStatus.idle
+            print("Failed to create file: \(error)")
+            return
         }
     }
     
-    var body: some View {
-        VStack {
-            Button {
-                isRecording = true
-                getRecorder()
-                
-                do {
-                    try AVAudioSession.sharedInstance().setActive(true)
-                } catch {
-                    print("Failed to set up audio session: \(error)")
-                    isRecording = false
-                    return
-                }
-                
-                let success = recorder?.record(forDuration: maxTimerLength)
-                Task { @MainActor in
-                    try? await Task.sleep(for: .seconds(maxTimerLength))
-                    recorder?.stop()
-                }
-                
-                recordingStartTime = Date()
-
-                printWithNewlineAbove(input: "Did start recording: \(success ?? false)")
-            } label: {
-                Image(systemName: isRecording ? "mic.fill" : "mic")
-            }
-            .padding(.bottom, 20)
-            
-            if isRecording {
-                HStack(spacing: 10) {
-                    Button("Stop") {
-                        recorder?.stop()
-                        isRecording = false
-                        recordingStartTime = nil
-                    }
-                    
-                    if let recordingStartTime = recordingStartTime {
-                        TimelineView(
-                            .periodic(from: .now, by: 0.1)
-                        ) { context in
-                            let timeSinceRecordStart = context.date.timeIntervalSince(recordingStartTime)
-                                 
-//                            if (timeSinceRecordStart < maxTimerLength) {
-                                let recordingLength = String(format: "%02.2f", timeSinceRecordStart)
-                                Text(recordingLength)
-//                            }
-//                             } else {
-//                                 recorder?.stop()
-//                             }
-                        }
-                    }
-                }
-            }
-            
-            AudioPlayer(audioURL: try! getSoundFileURL())
-        }
-        .onAppear {
-            recorderDelegate.onFinish = { _ in
-                isRecording = false
-                recordingStartTime = nil
-            }
-        }
+    func stopRecording() {
+        status = RecorderStatus.transitioning
+        engine.inputNode.removeTap(onBus: 0)
+        engine.stop()
+        audioFile = nil
+        status = RecorderStatus.idle
     }
 }
---
+++ tea-time/AudioRecorder/recorder-status.swift
@@ -0,0 +1,5 @@
+enum RecorderStatus {
+    case idle
+    case transitioning
+    case recording
+}
